8d939ae28d1b8220fbf2a139cd33b6229dde1326
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-29 14:29:08 -0700

    Added routes to new pages using createRoutesFromElements react-router-dom util

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import Root from './routes/root';
-import { ErrorPage, AboutPage } from './pages';
+import {
+  AboutPage,
+  ContactPage,
+  ErrorPage,
+  ExperiencePage,
+  HomePage,
+} from './pages';
 
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
+      <Route index element={<HomePage />} />
+      <Route path='about' element={<AboutPage />} />
+      <Route path='experience' element={<ExperiencePage />} />
+      <Route path='contact' element={<ContactPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
