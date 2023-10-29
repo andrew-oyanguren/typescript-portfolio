e6eaca9829fec802c3f52bcaec87ca4ddcd8e29c
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-29 13:30:06 -0700

    Updated pages exports from single index file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
+export { default as AboutPage } from './AboutPage/AboutPage';
+export { default as ExperiencePage } from './ExperiencePage/ExperiencePage';
+export { default as ContactPage } from './ContactPage/ContactPage';
