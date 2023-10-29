29d4ef09d3d2140e29d8e31ac76f61faf48a8d6f
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-29 15:09:06 -0700

    Added Navigation component to Root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
+import { Navigation } from '../components';
+
 export default function Root() {
   return (
     <>
       <p>Root Element Component</p>
+      <Navigation />
       <Outlet />
     </>
   );
