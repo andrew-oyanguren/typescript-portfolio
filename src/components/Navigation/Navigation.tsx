d3644ec7ab36dfdd1874964b64197a577eb17af4
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-30 22:58:28 -0700

    updated Navigation to include icons and added styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,19 +1,26 @@
 import { NavLink } from 'react-router-dom';
 
-import { ROUTES_CONFIG } from 'src/constants';
+import { NAV_CONFIG } from 'src/constants';
 
 import styles from './Navigation.module.css';
 
 export default function Navigation() {
   return (
-    <div className={styles.Navigation}>
-      <nav>
-        {ROUTES_CONFIG.map((obj) => (
-          <NavLink key={obj.path} to={obj.path}>
-            {obj.asset}
-          </NavLink>
-        ))}
-      </nav>
-    </div>
+    <nav className={styles.Navigation}>
+      {NAV_CONFIG.map((obj) => (
+        <NavLink key={obj.path} to={obj.path}>
+          {({ isActive }) => (
+            <div className={styles.icon}>
+              {isActive ? <span className={styles.selected}></span> : null}
+              <img
+                className={isActive ? styles.active : styles.inactive}
+                src={obj.asset}
+                alt={obj.altText}
+              />
+            </div>
+          )}
+        </NavLink>
+      ))}
+    </nav>
   );
 }
