6f7ca402e64e8e404f0b97cba3602d44837401ae
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-31 12:38:26 -0700

    renamed PageWrapper component to ResponsiveWrapper

--- src/components/ResponsiveWrapper/ResponsiveWrapper.tsx
+++ src/components/ResponsiveWrapper/ResponsiveWrapper.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react';
 
-import styles from './PageWrapper.module.css';
+import styles from './ResponsiveWrapper.module.css';
 
 type PageWrapperProps = {
   children: ReactNode;
 };
 
 export default function PageWrapper({ children }: PageWrapperProps) {
-  return <div className={styles.PageWrapper}>{children}</div>;
+  return <div className={styles.ResponsiveWrapper}>{children}</div>;
 }
