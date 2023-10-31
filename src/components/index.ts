784210e204f2ddf4670f5273987c7af4e2b8f325
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-31 12:39:27 -0700

    Updated imports for newly named ResponsiveWrapper component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as PageWrapper } from './PageWrapper/PageWrapper';
+export { default as ResponsiveWrapper } from './ResponsiveWrapper/ResponsiveWrapper';
 export { default as Navigation } from './Navigation/Navigation';
 export { default as Footer } from './Footer/Footer';
