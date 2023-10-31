0433c7588f7c637de8c2eb2a61e7f95d7e3c21ae
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-31 14:34:49 -0700

    Added ImageView component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,10 @@
-import { ResponsiveWrapper } from 'src/components';
+import { ResponsiveWrapper, ImageView } from 'src/components';
 
 export default function HomePage() {
   return (
     <ResponsiveWrapper>
       <h1>Home Page</h1>
+      <ImageView />
     </ResponsiveWrapper>
   );
 }
