96a2efd4bb538281364916b3ce3f533c7ab24d2d
andrew-oyanguren <aaoyanguren@outlook.com>
2023-10-31 11:26:20 -0700

    Added RenerErrorMessageType for renderErrorMessage util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,17 @@
 import { isRouteErrorResponse } from 'react-router-dom';
 
-export const renderErrorMessage = (error: unknown) => {
+type RenderErrorMessageType = {
+  data: string;
+  error: {
+    message: string;
+    stack: string;
+  };
+  internal: string;
+  status: number;
+  statusText: string;
+};
+
+export const renderErrorMessage = (error: RenderErrorMessageType | unknown) => {
   if (isRouteErrorResponse(error)) {
     console.log(error);
     return error.statusText;
