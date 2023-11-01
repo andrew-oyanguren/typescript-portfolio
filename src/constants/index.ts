761e20380e66780d78d5164b2ff5f9eb24c21980
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-01 13:36:25 -0700

    Abstracted IMAGE_CONFIG constants into new imageConstants file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
+import { IMAGE_CONFIG } from './imageConstants';
 import { PATH_NAMES, NAV_CONFIG } from './routerConstants';
 import { SOCIAL_LINKS } from './socialsConstants';
 
-export { PATH_NAMES, NAV_CONFIG, SOCIAL_LINKS };
+export { IMAGE_CONFIG, PATH_NAMES, NAV_CONFIG, SOCIAL_LINKS };
