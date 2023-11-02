08da82aeb281cbaa73b744868d237cc523c3290f
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-02 00:18:08 -0700

    Added ImageCarousel to HomePage component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,16 @@
-import { ResponsiveWrapper, ImageView } from 'src/components';
+import { ResponsiveWrapper, ImageView, ImageCarousel } from 'src/components';
 
 import useWindowDim from 'src/hooks/useWindowDim';
 
 export default function HomePage() {
   const { windowDim } = useWindowDim();
 
-  const isScreenMobile = windowDim.width < 600;
+  const isScreenMobile = windowDim.width < 540;
 
   return (
     <ResponsiveWrapper>
       <h1>Hi, I'm Andy.</h1>
-      {isScreenMobile ? <ImageView /> : <p>Image Carousel Placeholder</p>}
+      {isScreenMobile ? <ImageView /> : <ImageCarousel />}
     </ResponsiveWrapper>
   );
 }
