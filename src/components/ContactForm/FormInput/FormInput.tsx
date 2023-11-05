889c92bfe15dae6f70b44fc4a98c4b32f39ec7c4
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-05 12:58:52 -0800

    Abstarcted helpers from FormInput component

--- src/components/ContactForm/FormInput/FormInput.tsx
+++ src/components/ContactForm/FormInput/FormInput.tsx
@@ -1,41 +1,5 @@
-import { createElement } from 'react';
+import { renderInputElement, InputConfig } from '../helpers/helpers';
 
-import { INPUT_ELEMENTS } from '../constants';
-
-type InputTypeKeys = keyof typeof INPUT_ELEMENTS;
-type InputTypeValues = (typeof INPUT_ELEMENTS)[InputTypeKeys];
-
-type InputConfig = {
-  type: InputTypeValues;
-  props?: { type?: string; placeholder?: string } | null;
-  children?: { options: { value: string; title: string }[] };
-};
-
-type FormInputProps = {
-  inputConfig: InputConfig;
-};
-
-const renderInputElement = (inputConfig: InputConfig) => {
-  switch (inputConfig.type) {
-    case INPUT_ELEMENTS.TEXT:
-      return createElement(inputConfig.type, { ...inputConfig.props });
-    case INPUT_ELEMENTS.SELECT:
-      return createElement(
-        inputConfig.type,
-        { ...inputConfig.props },
-        inputConfig?.children?.options.map(({ value, title }) => (
-          <option key={value} value={value}>
-            {title}
-          </option>
-        ))
-      );
-    case INPUT_ELEMENTS.TEXT_AREA:
-      return createElement(inputConfig.type, { ...inputConfig.props });
-    default:
-      return null;
-  }
-};
-
-export default function FormInput({ inputConfig }: FormInputProps) {
-  return renderInputElement(inputConfig);
+export default function FormInput({ inputConfig }: InputConfig) {
+  return <div>{renderInputElement({ inputConfig })}</div>;
 }
