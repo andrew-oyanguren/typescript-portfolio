31a8c73629cd8534d5c63bc0d780d6accb646d2b
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-08 14:12:55 -0800

    Added Drawer and Hamburger components

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import Hamburger from './Hamburger/Hamburger';
+
+import styles from './Drawer.module.css';
+
+export default function Drawer() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <div className={`${styles.Drawer} ${isOpen ? styles.open : ''}`}>
+      <div
+        style={{
+          display: 'flex',
+          position: 'absolute',
+          top: '30%',
+          right: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <p style={{ fontSize: 36 }}>Toolbox</p>
+        <Hamburger onToggleDrawer={onToggleDrawer} isActive={isOpen} />
+      </div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
