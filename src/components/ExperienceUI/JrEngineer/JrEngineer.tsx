730535222347e86f1f787571a629a5f6319aea58
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-13 15:03:37 -0800

    Added styles for JrEngineer UI component

--- src/components/ExperienceUI/JrEngineer/JrEngineer.tsx
+++ src/components/ExperienceUI/JrEngineer/JrEngineer.tsx
@@ -3,14 +3,24 @@ import CodeIcon from 'src/assets/svgs/code.svg';
 import MapIcon from 'src/assets/svgs/map.svg';
 import { Card } from 'src/components';
 
+import styles from './JrEngineer.module.css';
+
 export default function JrEngineer() {
   return (
-    <>
-      <Card style={{ backgroundColor: 'var(--color-dark-gray)' }}>
+    <div className={styles.Container}>
+      <Card style={{ backgroundColor: 'var(--color-gray)' }}>
         <img style={{ width: 45, height: 45 }} src={CodeIcon} alt='Code Icon' />
-        <p>Responsible for Pulse releases and debugging.</p>
+        <p style={{ color: 'var(--color-dark)' }}>
+          Responsible for Pulse{' '}
+          <span style={{ color: 'var(--color-secondary)', fontWeight: 'bold' }}>
+            releases and debugging
+          </span>
+          .
+        </p>
       </Card>
-      <p>image placeholder</p>
+
+      <p>GitHub Canvas Placeholder</p>
+
       <Card
         style={{
           borderTopLeftRadius: 50,
@@ -19,22 +29,44 @@ export default function JrEngineer() {
           borderBottomLeftRadius: 0,
         }}
       >
-        <p>Developed a Q&A and release process for Pulse to the Play Store.</p>
+        <p>
+          Developed a{' '}
+          <span style={{ color: 'var(--color-dark)', fontWeight: 'bold' }}>
+            Q&A
+          </span>{' '}
+          and release process for Pulse to the Play Store.
+        </p>
         <img style={{ width: 45, height: 45 }} src={MapIcon} alt='Map Icon' />
-        <p>I was Instrumental in FE decisions when building new features.</p>
+        <p>
+          I was Instrumental in FE decisions when building{' '}
+          <span style={{ color: 'var(--color-dark)', fontWeight: 'bold' }}>
+            new features
+          </span>
+          .
+        </p>
       </Card>
-      <p>
-        Led refactor updating Pulse from its primitive state into an updated
-        React Native application
-      </p>
+
+      <div className={styles.textContainer}>
+        <p>
+          Led refactor updating Pulse from its primitive state into an updated
+          <span> React Native</span> application
+        </p>
+      </div>
+
       <Card style={{ backgroundColor: 'var(--color-dark)' }}>
         <img style={{ width: 45, height: 45 }} src={BookIcon} alt='Book Icon' />
         <p>
           Researched and successfully integrated React libraries into Novo.
-          Developed data grids, and forms using React Hook Form and
-          ReactDataGrid
+          Developed data grids, and forms using{' '}
+          <span style={{ color: 'var(--color-primary)', fontWeight: 'bold' }}>
+            React-Hook-Form
+          </span>{' '}
+          and{' '}
+          <span style={{ color: 'var(--color-primary)', fontWeight: 'bold' }}>
+            React-DataGrid
+          </span>
         </p>
       </Card>
-    </>
+    </div>
   );
 }
--- src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
+++ src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
@@ -7,12 +7,14 @@ export default function WebDevBody() {
   return (
     <div className={styles.Container}>
       <img src={PortfolioDesktop} alt='Portfolio Desktop View' />
+
       <p className={styles.description}>
         During this period, I created three portfolio websites, a card game for
         my toddler, a single page application for an English tutor, and a
         website for a fire prevention company as well as multiple other personal
         projects.
       </p>
+
       <img src={PortfolioMobile} alt='Portfolio Mobile View' />
     </div>
   );
