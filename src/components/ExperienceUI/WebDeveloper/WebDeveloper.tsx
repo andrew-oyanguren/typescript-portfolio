e308391497829201d547316f56d4fda413f60460
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-13 14:47:41 -0800

    Added styles for the WebDeveloper UI component

--- src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
+++ src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
@@ -1,13 +1,13 @@
 import PortfolioMobile from 'src/assets/images/portfolio-mobile.png';
 import PortfolioDesktop from 'src/assets/images/portfolio-desktop.png';
 
+import styles from './WebDeveloper.module.css';
+
 export default function WebDevBody() {
   return (
-    <div
-      style={{ backgroundColor: 'var(--color-dark-gray)', padding: '40px 0' }}
-    >
+    <div className={styles.Container}>
       <img src={PortfolioDesktop} alt='Portfolio Desktop View' />
-      <p style={{ padding: '40px 0px' }}>
+      <p className={styles.description}>
         During this period, I created three portfolio websites, a card game for
         my toddler, a single page application for an English tutor, and a
         website for a fire prevention company as well as multiple other personal
