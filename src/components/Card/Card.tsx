b0c8dfce5b6f1b10d7e1f088806ba12abf980a46
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-25 13:45:16 -0800

    Added contextual comment to Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,6 +7,12 @@ type CardProps = {
   style?: CSSProperties;
 };
 
+/* 
+  The Card style prop allows for unique card styling.
+  style must be a style object:
+  {color: 'red', padding: 20}
+*/
+
 export default function Card({ children, style }: CardProps) {
   return (
     <div style={style} className={styles.Card}>
