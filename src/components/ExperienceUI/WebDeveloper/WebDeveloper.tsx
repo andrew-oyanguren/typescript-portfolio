ac342fe58348fa6dc144ae06dd7ab7758dd6d9e9
andrew-oyanguren <aaoyanguren@outlook.com>
2023-11-29 13:47:34 -0800

    Deleted portfolio-mobile asset and update WebDeveloper component to support medium devices

--- src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
+++ src/components/ExperienceUI/WebDeveloper/WebDeveloper.tsx
@@ -1,29 +1,34 @@
-import PortfolioMobile from 'src/assets/images/portfolio-mobile.png';
 import PortfolioDesktop from 'src/assets/images/portfolio-desktop.png';
 
 import styles from './WebDeveloper.module.css';
 
 export default function WebDevBody() {
   return (
-    <>
-      <img
-        className={styles.WebDevImg}
-        src={PortfolioDesktop}
-        alt='Portfolio Desktop View'
-      />
+    <div className={styles.WebDevContainer}>
+      <div className={styles.WebDevImgWrapper}>
+        <img
+          className={styles.WebDevImg}
+          src={PortfolioDesktop}
+          alt='Portfolio Desktop View'
+        />
+      </div>
 
-      <p>
-        During this period, I created three portfolio websites, a card game for
-        my toddler, a single page application for an English tutor, and a
-        website for a fire prevention company as well as multiple other personal
-        projects.
-      </p>
+      <div className={styles.WebDevTextContainer}>
+        <p>
+          During this period, I created three portfolio websites, a card game
+          for my toddler, a single page application for an English tutor, and a
+          website for a fire prevention company as well as multiple other
+          personal projects.
+        </p>
+      </div>
 
-      <img
-        className={styles.WebDevImg}
-        src={PortfolioMobile}
-        alt='Portfolio Mobile View'
-      />
-    </>
+      <div className={styles.WebDevImgWrapper}>
+        <img
+          className={styles.WebDevImg}
+          src={PortfolioDesktop}
+          alt='Portfolio Mobile View'
+        />
+      </div>
+    </div>
   );
 }
