bc5ee1e5d23fb056dcacf2747dcfbd3aa594f64a
andrew-oyanguren <aaoyanguren@outlook.com>
2023-12-14 01:09:54 -0800

    Added BookList CSS and added list title

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,7 +1,18 @@
+import { BOOKS_LIST_CONFIG } from 'src/constants';
+
+import styles from './BooksList.module.css';
+
 export default function BooksList() {
   return (
-    <aside>
-      <ul></ul>
-    </aside>
+    <section className={styles.BooksListContainer}>
+      <h2 className={styles.BooksListTitle}>Here's what I'm reading</h2>
+      <ul className={styles.BooksList}>
+        {BOOKS_LIST_CONFIG.map(({ altText, asset }) => (
+          <li key={altText} className={styles.ImageContainer}>
+            <img src={asset} alt={altText} />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
