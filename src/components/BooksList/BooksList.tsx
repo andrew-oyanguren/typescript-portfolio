10c100c08b6efb1268249e5a2b09fb5517193405
andrew-oyanguren <aaoyanguren@outlook.com>
2023-12-15 23:23:19 -0800

    Updated BooksList animation to incorporate useInView hook

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,9 +1,12 @@
+import { useInView } from 'react-intersection-observer';
 import { BOOKS_LIST_CONFIG } from 'src/constants';
 import BookOpenIcon from 'src/assets/svgs/book-open.svg';
 
 import styles from './BooksList.module.css';
 
 export default function BooksList() {
+  const { ref, inView } = useInView({ threshold: 0.9 });
+
   return (
     <section className={styles.BooksListContainer}>
       <div className={styles.BooksListTitle}>
@@ -11,7 +14,10 @@ export default function BooksList() {
         <h2>Here's what I'm reading</h2>
       </div>
 
-      <ul className={styles.BooksList}>
+      <ul
+        ref={ref}
+        className={`${styles.BooksList} ${inView ? styles.animate : ''}`}
+      >
         {BOOKS_LIST_CONFIG.map(({ altText, asset }) => (
           <li key={altText} className={styles.ImageContainer}>
             <img src={asset} alt={altText} />
