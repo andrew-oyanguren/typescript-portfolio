31d1779cf76b1621dee8cbab81cfd8e4b9183def
andrew-oyanguren <aaoyanguren@outlook.com>
2023-12-16 14:44:38 -0800

    Increased ImageView title font-size

--- src/components/ImageView/ImageView.tsx
+++ src/components/ImageView/ImageView.tsx
@@ -7,7 +7,12 @@ export default function ImageView() {
     <div className={styles.ImageView}>
       {IMAGE_CONFIG.map((obj) => (
         <div key={obj.asset} className={styles.ImageContainer}>
-          <p className={styles.title}>{obj.title}</p>
+          <p
+            style={{ fontSize: 'var(--font-size-medium)' }}
+            className={styles.title}
+          >
+            {obj.title}
+          </p>
           <div className={styles.ImageWrapper}>
             <img src={obj.asset} alt={obj.altText} />
           </div>
