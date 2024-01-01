41e6a5989a99d65603212ef611f8cd384fab23dd
andrew-oyanguren <aaoyanguren@outlook.com>
2024-01-01 13:49:48 -0800

    added conditional Circle CSS styles to the ExperienceBar

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,9 +1,24 @@
+import useWindowDim from 'src/hooks/useWindowDim';
+import { BALL_POSITION, DEVICE_WIDTHS } from 'src/constants';
+import { getBallVariant } from './helpers';
+
 import styles from './ExperienceBar.module.css';
 
-export default function ExperienceBar({ circlePosition = 'top' }) {
+export default function ExperienceBar({ circlePosition = BALL_POSITION.TOP }) {
+  const { windowDim } = useWindowDim();
+  const isLargeDevice = windowDim.width >= DEVICE_WIDTHS.LARGE;
+
   return (
     <div className={styles.ExperienceBar}>
-      <div style={{ gridArea: circlePosition }} className={styles.Circle} />
+      <div
+        style={{
+          gridArea: circlePosition,
+          backgroundColor: isLargeDevice
+            ? getBallVariant(circlePosition)
+            : 'var(--color-secondary)',
+        }}
+        className={styles.Circle}
+      />
       <div className={styles.Line} />
       <div className={styles.Line} />
     </div>
