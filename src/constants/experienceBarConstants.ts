b287779c4e069569bb0b6fdfa47f6d77d6c01362
andrew-oyanguren <aaoyanguren@outlook.com>
2024-01-01 13:18:05 -0800

    Added experienceBarConstants

---
+++ src/constants/experienceBarConstants.ts
@@ -0,0 +1,7 @@
+const BALL_POSITION = Object.freeze({
+  TOP: 'top',
+  MIDDLE: 'middle',
+  BOTTOM: 'bottom',
+});
+
+export { BALL_POSITION };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,13 @@
 import { ABOUT_CARDS_CONFIG, ICON_ALT_TEXT } from './aboutCardConstants';
 import { BOOKS_LIST_CONFIG } from './BooksListConstants';
+import { BALL_POSITION } from './experienceBarConstants';
 import { IMAGE_CONFIG } from './imageConstants';
 import { PATH_NAMES, NAV_CONFIG } from './routerConstants';
 import { SOCIAL_LINKS } from './socialsConstants';
 
 export {
   ABOUT_CARDS_CONFIG,
+  BALL_POSITION,
   BOOKS_LIST_CONFIG,
   ICON_ALT_TEXT,
   IMAGE_CONFIG,
