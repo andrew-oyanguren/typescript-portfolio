eb001b86877e87e00c57178f8c3af75d3fd99518
andrew-oyanguren <aaoyanguren@outlook.com>
2024-01-01 14:44:54 -0800

    Added prop types to ExperienceBar component

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,10 +1,17 @@
 import useWindowDim from 'src/hooks/useWindowDim';
 import { BALL_POSITION, DEVICE_WIDTHS } from 'src/constants';
+import { BallPositionType } from 'src/constants/experienceBarConstants';
 import { getBallVariant } from './helpers';
 
 import styles from './ExperienceBar.module.css';
 
-export default function ExperienceBar({ circlePosition = BALL_POSITION.TOP }) {
+type ExperienceBarPropTypes = {
+  circlePosition: BallPositionType;
+};
+
+export default function ExperienceBar({
+  circlePosition = BALL_POSITION.TOP,
+}: ExperienceBarPropTypes) {
   const { windowDim } = useWindowDim();
   const isLargeDevice = windowDim.width >= DEVICE_WIDTHS.LARGE;
 
